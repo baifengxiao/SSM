ed759223f34deeeacb0932a46c41d57f91558a30
yupengtao <javaer@111.com>
2024-01-18 12:16:47 +0800

    2.Spring:引入Log4j2日志

--- spring6-first/src/main/java/com/this0/spring6/bean/HelloWorldTest.java
+++ spring6-first/src/main/java/com/this0/spring6/bean/HelloWorldTest.java
@@ -1,17 +1,23 @@
 package com.this0.spring6.bean;
 
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
 public class HelloWorldTest {
+
+    private Logger logger = LoggerFactory.getLogger(HelloWorldTest.class);
     @Test
     public void testHelloWorld(){
 
         ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
         HelloWorld helloWorld = (HelloWorld) ac.getBean("helloWorld");
         helloWorld.sayHello();
+
+        logger.info("执行成功");
     }
     
 
