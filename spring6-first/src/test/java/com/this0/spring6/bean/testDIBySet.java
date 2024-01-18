2796e4dbde02dfc125ffce67c016c4c528952d28
yupengtao <javaer@111.com>
2024-01-18 20:32:18 +0800

    依赖注入之setter注入

---
+++ spring6-first/src/test/java/com/this0/spring6/bean/testDIBySet.java
@@ -0,0 +1,14 @@
+package com.this0.spring6.bean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class testDIBySet {
+    @Test
+    public void testDIBySet(){
+        ApplicationContext ac = new ClassPathXmlApplicationContext("spring-di.xml");
+        Student studentOne = ac.getBean("studentOne", Student.class);
+        System.out.println(studentOne);
+    }
+}
