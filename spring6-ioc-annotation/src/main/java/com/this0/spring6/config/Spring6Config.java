9bbf103f7c038fd335344092128830367aa72878
yupengtao <javaer@111.com>
2024-01-20 20:37:46 +0800

    测试Spring全注解开发

---
+++ spring6-ioc-annotation/src/main/java/com/this0/spring6/config/Spring6Config.java
@@ -0,0 +1,9 @@
+package com.this0.spring6.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.this0.spring6")
+public class Spring6Config {
+}
--- spring6-ioc-annotation/src/test/java/com/this0/spring6/UserTest.java
+++ spring6-ioc-annotation/src/test/java/com/this0/spring6/UserTest.java
@@ -1,20 +1,20 @@
 package com.this0.spring6;
 
+import com.this0.spring6.config.Spring6Config;
 import com.this0.spring6.controller.UserController;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UserTest {
-    private Logger logger = LoggerFactory.getLogger(UserTest.class);
 
     @Test
     public void testAnnotation(){
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(Spring6Config.class);
         UserController userController = context.getBean("userController", UserController.class);
         userController.out();
-        logger.info("执行成功");
     }
 }
