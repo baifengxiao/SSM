4c81fc9608a987ca2ae0a6d0cb6056f1a87d1d73
yupengtao <javaer@111.com>
2024-01-30 22:07:56 +0800

    springmvc参数名匹配传值

---
+++ springmvc/src/main/java/com/this0/springmvc/controller/ParamController.java
@@ -0,0 +1,27 @@
+package com.this0.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("param")
+public class ParamController {
+
+    /**
+     * 前端请求: http://localhost:8080/param/value?name=xx&age=18
+     *
+     * 可以利用形参列表,直接接收前端传递的param参数!
+     *    要求: 参数名 = 形参名
+     *          类型相同
+     * 出现乱码正常，json接收具体解决！！
+     * @return 返回前端数据
+     */
+    @GetMapping(value="/value")
+    @ResponseBody
+    public String setupForm(String name,int age){
+        System.out.println("name = " + name + ", age = " + age);
+        return name + age;
+    }
+}
