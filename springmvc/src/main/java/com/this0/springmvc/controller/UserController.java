001bd5e909a70b085d1b38e142dbe99d60314468
yupengtao <javaer@111.com>
2024-01-30 21:44:41 +0800

    精确路径匹配

---
+++ springmvc/src/main/java/com/this0/springmvc/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.this0.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+    /**
+     * 精准设置访问地址 /user/login
+     */
+    @RequestMapping(value = {"/user/login"})
+    @ResponseBody
+    public String login(){
+        System.out.println("UserController.login");
+        return "login success!!";
+    }
+
+    /**
+     * 精准设置访问地址 /user/register
+     */
+    @RequestMapping(value = {"/user/register"})
+    @ResponseBody
+    public String register(){
+        System.out.println("UserController.register");
+        return "register success!!";
+    }
+
+}
+
