46daf8eabfb673e0e56cd0cc5a823ede7c24786a
yupengtao <javaer@111.com>
2024-03-06 03:12:00 +0800

    SpringMVC处理静态资源

--- springmvc/src/main/java/com/this0/springmvc/config/SpringMvcConfig.java
+++ springmvc/src/main/java/com/this0/springmvc/config/SpringMvcConfig.java
@@ -2,6 +2,7 @@ package com.this0.springmvc.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,4 +11,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan(basePackages = "com.this0.springmvc")
 public class SpringMvcConfig implements WebMvcConfigurer {
 
+    //开启静态资源处理
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        WebMvcConfigurer.super.configureDefaultServletHandling(configurer);
+        configurer.enable();
+    }
 }
